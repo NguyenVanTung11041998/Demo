2e8b7bb84e43d1ae77f8370be1e591861df0184a
tuanda24 <dat.2000.dz@gmial.com>
2023-10-03 22:29:12 +0700

    .

---
+++ DemoWebApi/Repositories/CompanyPostHashtag/CompanyPostHashtagRepository.cs
@@ -0,0 +1,9 @@
+﻿using DemoWebApi.EFCore;
+
+namespace DemoWebApi.Repositories.CompanyPostHashtag
+{
+    public class CompanyPostHashtagRepository :Repository<Entities.CompanyPostHashtag>,ICompanyPostHashtagRepository
+    {
+        public CompanyPostHashtagRepository(ApplicationDbContext context) : base(context) { }
+    }
+}
---
+++ DemoWebApi/Repositories/CompanyPostHashtag/ICompanyPostHashtagRepository.cs
@@ -0,0 +1,8 @@
+﻿using DemoWebApi.Entities;
+
+namespace DemoWebApi.Repositories.CompanyPostHashtag
+{
+    public interface ICompanyPostHashtagRepository : IRepository<Entities.CompanyPostHashtag>
+    {
+    }
+}
